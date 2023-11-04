172bd9bc81dbdd2cc44023c62e90d8a1f6124e68
Anshika <anshikapathak65@gmail.com>
2023-11-04 11:38:56 +0530

    upload bakery website

---
+++ src/data/data.js
@@ -0,0 +1,51 @@
+import Chocolate from "../images/chocolate.jpeg";
+import clinkers from "../images/clinkers.jpeg";
+import unicorn from "../images/unicorn.jpeg";
+import donuts from "../images/donuts.jpeg";
+import icecream from "../images/icecream.jpeg";
+import beverages from "../images/beverages.jpeg";
+
+export const MenuList = [
+  {
+    name: "Chocolate Cake",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, ab!",
+    image: Chocolate,
+    price: 200,
+  },
+  {
+    name: "Clinker's Cake",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, ab!",
+    image: clinkers,
+    price: 250,
+  },
+  {
+    name: "Unicorn Cake",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, ab!",
+    image: unicorn,
+    price: 300,
+  },
+  {
+    name: "donuts",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, ab!",
+    image: donuts,
+    price: 100,
+  },
+  {
+    name: "icecream",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, ab!",
+    image: icecream,
+    price: 400,
+  },
+  {
+    name: "beverages",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, ab!",
+    image: beverages,
+    price: 500,
+  },
+];
